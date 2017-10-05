5bffbb29d306cea363121b644889a6e7bd858f43
György Mátyás <gy.matyas97@gmail.com>
2017-10-05 19:04:52 +0200

    residents data

--- static/js/dom.js
+++ static/js/dom.js
@@ -1,5 +1,8 @@
 dom = {
+
     page: 1,
+
+    pagePlanets: null,
 
     createPlanetsTable: function (planets) {
         let tableHTML = `<thead>
@@ -10,10 +13,11 @@ dom = {
                                 <th>Terrain</th>
                                 <th>Surface Water Percentage</th>
                                 <th>Population</th>
+                                <th>Residents</th>
                             </tr>
                         </thead>`;
         for (let i = 0; i < planets.length; i++) {
-            tableHTML += `<tr>
+            tableHTML += `<tr id="planet-${i}">
                             <td>${planets[i].name}</td>
                             <td>${planets[i].diameter}</td>
                             <td>${planets[i].climate}</td>
@@ -24,6 +28,12 @@ dom = {
             }
             else {
                 tableHTML += `<td>${planets[i].population}</td>`
+            }
+            if (planets[i].residents.length > 0) {
+                tableHTML += `<td class="residentsCol"><button class="btn btn-dark" data-planetId="${i}">${planets[i].residents.length}</button></td>`
+            }
+            else {
+                tableHTML += '<td></td>'
             }
             tableHTML += `</tr>`
         }
@@ -37,36 +47,77 @@ dom = {
             type: "GET"
         }).done(
             function (data) {
-                console.log("Ajax length: " + data.results.length);
-                $('#planetsTable').html(dom.createPlanetsTable(data.results));
-                dom.bindPrevButtonFunction();
-                dom.bindNextButtonFunction(data.results.length);
+                dom.pagePlanets = data.results;
+                $('#planetsTable').html(dom.createPlanetsTable(dom.pagePlanets));
+                dom.buttons.bindPrevButtonFunction();
+                dom.buttons.bindNextButtonFunction(dom.pagePlanets.length);
             })
     },
 
-    bindNextButtonFunction: function (length) {
-        $('#next').unbind('click');
-        console.log("Page: " + dom.page);
-        console.log("Function length: " + length)
-        if (length === 10) {
-            $('#next').removeAttr("disabled");
-            $('#next').bind('click', function () {
-                dom.page += 1;
-                dom.getPlanetsAjaxCall();
-            })
-        } else { $('#next').attr("disabled", "disabled") };
+    buttons: {
+
+        bindNextButtonFunction: function (length) {
+            $('#next').unbind('click');
+            if (length === 10) {
+                $('#next').removeAttr("disabled");
+                $('#next').bind('click', function () {
+                    dom.page += 1;
+                    dom.getPlanetsAjaxCall();
+                })
+            } else { $('#next').attr("disabled", "disabled") };
+        },
+
+        bindPrevButtonFunction: function () {
+            $('#previous').unbind('click');
+            if (dom.page > 1) {
+                $('#previous').removeAttr("disabled");
+                $('#previous').bind('click', function () {
+                    dom.page--;
+                    dom.getPlanetsAjaxCall();
+                })
+            }
+            else { $('#previous').attr("disabled", "disabled") };
+        }
     },
 
-    bindPrevButtonFunction: function () {
-        $('#previous').unbind('click');
-        if (dom.page > 1) {
-            $('#previous').removeAttr("disabled");
-            $('#previous').bind('click', function() {
-                dom.page --;
-                dom.getPlanetsAjaxCall();
+    getResidentsForPlanet: function (planetId) {
+        for (let j = 0; j < dom.pagePlanets[planetId].residents.length; j++) {
+            $.getJSON(`${dom.pagePlanets[planetId].residents[j]}`, function (resident) {
+                $('#modalTable').append(`<tr>
+                                            <td>${resident.name}</td>
+                                            <td>${resident.height}</td>
+                                            <td>${resident.mass}</td>
+                                            <td>${resident.skin_color}</td>
+                                            <td>${resident.hair_color}</td>
+                                            <td>${resident.eye_color}</td>
+                                            <td>${resident.birth_year}</td>
+                                            <td>${resident.gender}</td>
+                                        </tr>`)
             })
         }
-        else { $('#previous').attr("disabled","disabled") };
+    },
+
+    getResidentAjaxCall: function (residentURL) {
+        var residentHTML = ""
+        $.ajax({
+            url: residentURL,
+            dataType: "json",
+            type: "GET"
+        }).done(
+            function (residentData) {
+                residentHTML =
+                    dom.residentsArray.push(residentHTML);
+            })
+    },
+
+
+    test: function () {
+        $.when(dom.getResidentsURLsForPlanet("0")).done(function () {
+            for (let i = 0; i < dom.residentURLArray.length; i++) {
+                dom.getResidentAjaxCall(dom.residentURLArray[i])
+
+            }
+        });
     }
 
 }
